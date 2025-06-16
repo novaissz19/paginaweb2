e68f30d47b2a1e8e1719a24a9b4ab10291274993
novaissz19 <166837188+novaissz19@users.noreply.github.com>
2025-06-16 07:54:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,19 @@
+criaCartao(
+    ' Geopolítica ',
+    'Qual é a capital da ONU, onde ocorrem as principais reuniões internacionais?
+    ' Nova York 
+
+criaCartao(
+    'Geografia',
+    'Quais são os cinco continentes habitados do planeta Terra?
+    África, Antártica, Ásia, Austrália, Europa
+)
+
+criaCartao(
+    'Corpo humano',
+    'Qual o maior órgão do corpo humano?',
+    ' Pele 
+
+criaCartao(
+    'Qual a montanha mais alta do mundo?',
+    '  Monte Everest
